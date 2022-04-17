8f053f9235ca2ad41616d3f76773920aa5f79e49
Iury2982 <iuryoliveiraqueiroz@alu.ufc.br>
2022-04-17 00:35:19 -0300

    Atualizando

---
+++ Atividades/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Questão01 from './Component/Questão01';
+
+function App() {
+    return ( <
+        div className = 'App' >
+        <
+        Questão01 / >
+        <
+        /div>
+    )
+}
+
+export default App;
